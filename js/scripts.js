6fb9d516165814d0f1f823ecd8f9452fcecaea42
Anthony Szymaniak <hello@eyeoftheant.com>
2019-02-24 22:02:59 +0000

    a lot

--- js/scripts.js
+++ js/scripts.js
@@ -6,8 +6,110 @@ $(document).ready(function() {
 	});
 });
 
-$(function(){
-  $("#menu-button").on("click", function(){
-    $("#menu-container").fadeToggle();
-  });
-})
+// $(function(){
+//   $("#menu-button").on("click", function(){
+//     $("#menu-container").slideToggle();
+//   });
+// })
+
+
+
+
+// $( "#menu-button" ).on ("click", function() {
+//   $( "#menu-container" ).slideToggle( "slow", function() {
+//   });
+//   // $( ".menu" ).active();
+//   // });
+// });
+//
+
+//
+// $.fn.slideFadeToggle = function(speed, easing, callback) {
+//     return this.animate({
+//         opacity: 'toggle',
+//         height: 'toggle'
+//     }, speed, easing, callback);
+// };
+//
+//
+// $( "#menu-button" ).on ("click", function() {
+//     $( "#menu-container" ).slideFadeToggle( "slow", function() {
+//       $( ".menu" ).delay( 2000 ).slideFadeToggle( 1000 );
+// });
+// });
+// });
+
+
+// $( "#menu-button" ).click(function() {
+// $( ".menu" ).toggle( 300 ).delay( 300 ).fadeIn( 400 );
+// $( "#menu-container" ).slideToggle( 400 );
+// });
+
+
+$("#menu-button").click(function() {
+  $('.menu-container').toggleClass('transform-active');
+  $('.menu').toggleClass('transform-active');
+});
+
+
+// var menuButton = document.querySelector('#menu-button');
+// var items = document.querySelectorAll('.menu-container');
+//
+// // get vendor transition property
+// var docElemStyle = document.documentElement.style;
+// var transitionProp = typeof docElemStyle.transition == 'string' ?
+//     'transition' : 'WebkitTransition';
+//
+// menuButton.onclick = function() {
+//   for ( var i=0; i < items.length; i++ ) {
+//     var item = items[i];
+//     // stagger transition with transitionDelay
+//     item.style[ transitionProp + 'Delay' ] = ( i * 50 ) + 'ms';
+//     item.classList.toggle('is-moved');
+//   }
+// };
+
+
+
+// $('#menu-button').click(function () {
+//         $('#menu-container').slideToggle("slow", function() {
+//             // $('.menu').fadeToggle("slow", function() {
+//             //   // $('.menu').animate({ transform: '1' }, 800, function() {
+//                 // });
+//             });
+//         });
+
+
+// $('#menu-button').click(function () {
+//         $('#menu-container').slideToggle("slow", function() {
+//             $('.menu').fadeTo( "slow", function() {
+//
+//                 });
+//             });
+//         });
+
+// $( '.menu' ).fadeTo( "slow", 0.33 () {
+
+
+// $( "#menu-button" ).on ("click", function() {
+//   $( ".menu" ).fade( "slow", function() {
+//     // Animation complete.
+//   });
+// });
+
+//
+//
+// $( "#menu-button" ).on ("click", function() {
+//   $( "#menu-container" ).slideToggle('2000', "easeOutBack", function() {
+//     // Animation complete.
+//   });
+// });
+
+
+
+  //
+  // $("#menu-container").on ("show", function(){
+  //   $("#menu").animate({
+  //     opacity: '0.5',
+  //   });
+  // });
