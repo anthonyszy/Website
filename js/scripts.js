3d71d2892d6bc242ddde6829b52f4011f01c8bf0
Anthony Szymaniak <hello@eyeoftheant.com>
2020-07-17 15:53:56 +0100

    a

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ $(document).ready(function() {
     $('#fullpage').fullpage({
 		sectionsColor: ['transparent', 'transparent', 'transparent'],
 		menu: '.main-nav ul',
+    navigation: true,
 
             easingcss3: 'cubic-bezier(0.645, 0.075, 0.33, 1)'
 	});
